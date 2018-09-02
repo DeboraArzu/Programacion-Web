6df85ac7f9417936e3c17745602572afdf01e438
DeboraArzu <deboranise29@hotmail.com>
2018-09-02 00:01:26 -0600

    new buttons

--- MyWeb/Functions.js
+++ MyWeb/Functions.js
@@ -1,5 +1,3 @@
-﻿
-function myFunction() {
-    document.getElementById("form1").style.backgroundColor = "yellow";
-    //document.getElementsByTagName("body").style.backgroundColor = "yellow"
+﻿function myFunction(color) {
+    document.body.style.backgroundColor = color;
 }
