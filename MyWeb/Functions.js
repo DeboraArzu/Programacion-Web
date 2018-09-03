f9f73727689928b989160d47c7a2328d38502fb0
DeboraArzu <deboranise29@hotmail.com>
2018-09-03 17:41:02 -0600

    slide show added

--- MyWeb/Functions.js
+++ MyWeb/Functions.js
@@ -1,3 +1,10 @@
 ﻿function myFunction(color) {
     document.body.style.backgroundColor = color;
 }
+function openSideNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
